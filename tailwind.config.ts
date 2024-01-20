36a4ba12ea49d2d658a5186d002167d9df4215fa
yutocreate <1otuy26@gmail.com>
2024-01-20 14:16:05 +0900

    不要な画像の削除と追加

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,12 @@ const config: Config = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      width: {
+        '300': '300px', '400': '400px',
+      },
+      height: {
+        '300': '300px', '400': '400px',
+      }
     },
   },
   plugins: [],
